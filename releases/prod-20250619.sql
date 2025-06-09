8c133ef303c5afb56436dd2191a5238ea22f93dc
karolkwasnikggs <112874100+karolkwasnikggs@users.noreply.github.com>
2025-06-09 19:18:43 +0000

    auto release prod-20250619

---
+++ releases/prod-20250619.sql
@@ -0,0 +1,37 @@
+/********************************************/
+-- AUTO RELEASE: prod-20250619
+/********************************************/
+
+----------------------------------------------
+-- SECTION: MIGRATIONS (from QA releases)
+----------------------------------------------
+-- FILE: changes/test123.sql
+sadasdasd
+
+
+
+-- FILE: changes/test91.sql
+test
+
+
+
+----------------------------------------------
+-- SECTION: SCHEMA (from QA releases)
+----------------------------------------------
+-- OBJECT: schema/functions/func.sql
+bla bla
+
+
+
+-- OBJECT: schema/functions/new_file.sql
+asdasdasdas
+
+
+asdasdsa
+
+
+
+alter table bla bla another bla
+
+
+
