b2e8bd310c0255b44fe7e6126624e875b63bced2
karolkwasnikggs <112874100+karolkwasnikggs@users.noreply.github.com>
2025-06-09 17:31:46 +0000

    auto release ${CURRENT_TAG}

---
+++ releases/prod-20250611.sql
@@ -0,0 +1,7 @@
+/********************************************/
+-- AUTO RELEASE: prod-20250611
+-- Includes changes from QA tags after: prod-20250610
+/********************************************/
+
+-- SECTION: MIGRATIONS (changes/)
+-- SECTION: SCHEMA
