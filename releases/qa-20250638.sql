2b073b3e485619ecf18eedb38dd22d707c4d86b1
karolkwasnikggs <112874100+karolkwasnikggs@users.noreply.github.com>
2025-06-09 19:17:49 +0000

    auto release qa-20250638

---
+++ releases/qa-20250638.sql
@@ -0,0 +1,39 @@
+/********************************************/
+-- AUTO RELEASE: qa-20250638
+/********************************************/
+
+----------------------------------------------
+-- SECTION: MIGRATIONS (changes/)
+----------------------------------------------
+-- FILE: changes/test123.sql
+
+sadasdasd
+
+
+
+----------------------------------------------
+-- SECTION: SCHEMA (FULL if first release)
+----------------------------------------------
+-- NEW OBJECT: schema/functions/func.sql
+
+bla bla
+
+
+
+-- MODIFIED OBJECTS (schema/) AS COMMENTS
+-- MODIFIED OBJECT: schema/functions/new_file.sql
+-- BEGIN DIFF --------------------------------
+-- diff --git a/schema/functions/new_file.sql b/schema/functions/new_file.sql
+-- index 20f0e53..a179ef2 100644
+-- --- a/schema/functions/new_file.sql
+-- +++ b/schema/functions/new_file.sql
+-- @@ -5,4 +5,4 @@ asdasdsa
+--  
+--  
+--  
+-- -alter table bla bla
+-- +alter table bla bla another bla
+-- END DIFF ----------------------------------
+
+
+
