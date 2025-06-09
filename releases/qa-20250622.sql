ef81facc8ba893cd2a48765ed58a1f15288e2a8a
karolkwasnikggs <112874100+karolkwasnikggs@users.noreply.github.com>
2025-06-09 17:53:59 +0000

    auto release qa-20250622

---
+++ releases/qa-20250622.sql
@@ -0,0 +1,30 @@
+/********************************************/
+-- AUTO RELEASE: qa-20250622
+/********************************************/
+
+----------------------------------------------
+-- SECTION: MIGRATIONS (changes/)
+----------------------------------------------
+-- FILE: changes/test.sql
+
+test123
+
+
+
+----------------------------------------------
+-- SECTION: SCHEMA (FULL if first release)
+----------------------------------------------
+-- NEW OBJECT: schema/functions/new_file.sql
+
+asdasdasdas
+
+
+asdasdsa
+
+
+
+alter table bla bla
+
+
+
+-- MODIFIED OBJECTS (schema/) AS COMMENTS
