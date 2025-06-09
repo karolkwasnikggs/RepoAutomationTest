1c1baad3417e9a325c3cf9c86578003e608f3b4a
karolkwasnikggs <112874100+karolkwasnikggs@users.noreply.github.com>
2025-06-09 18:54:50 +0200

    Update emp.sql

--- schema/tables/emp.sql
+++ schema/tables/emp.sql
@@ -1,2 +1,2 @@
 -- create
-CREATE TABLE emp (id INT PRIMARY KEY, name VARCHAR(50), salary NUMBER, eq_id NUMBER, rating NUMBER, mail VARCHAR2(50));
+CREATE TABLE emp (id INT PRIMARY KEY, name VARCHAR(50), salary NUMBER, eq_id NUMBER, rating NUMBER, mail VARCHAR2(50), phone NUMBER);
