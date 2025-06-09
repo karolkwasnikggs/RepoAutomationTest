10059cd8628b9d4906e10b9015eda0f3dd18cf26
karolkwasnikggs <112874100+karolkwasnikggs@users.noreply.github.com>
2025-06-09 20:40:06 +0000

    auto release prod-20250621

---
+++ releases/prod-20250621.sql
@@ -0,0 +1,25 @@
+/********************************************/
+-- AUTO RELEASE: prod-20250621
+/********************************************/
+
+----------------------------------------------
+-- SECTION: MIGRATIONS (from QA releases)
+----------------------------------------------
+-- FILE: changes/asdsadas.sql
+asdasdasd
+
+
+
+-- FILE: changes/teeeeest.sql
+asdasdsadas
+
+
+
+-- FILE: changes/testtest1.sql
+testsststs
+
+
+
+----------------------------------------------
+-- SECTION: SCHEMA (from QA releases)
+----------------------------------------------
