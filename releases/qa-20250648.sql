b642019899c6d00ca3c408cf287db614831186f4
karolkwasnikggs <112874100+karolkwasnikggs@users.noreply.github.com>
2025-06-09 21:04:47 +0000

    auto release qa-20250648

---
+++ releases/qa-20250648.sql
@@ -0,0 +1,30 @@
+/********************************************/
+-- AUTO RELEASE: qa-20250648
+/********************************************/
+
+----------------------------------------------
+-- SECTION: MIGRATIONS (changes/)
+----------------------------------------------
+-- FILE: changes/m1.sql
+m1
+
+
+
+----------------------------------------------
+-- SECTION: SCHEMA (FULL if first release)
+----------------------------------------------
+-- MODIFIED OBJECT: schema/procedures/insert_emp.sql
+-- BEGIN DIFF --------------------------------
+-- diff --git a/schema/procedures/insert_emp.sql b/schema/procedures/insert_emp.sql
+-- index 463d3eb..4a40fa4 100644
+-- --- a/schema/procedures/insert_emp.sql
+-- +++ b/schema/procedures/insert_emp.sql
+-- @@ -12,3 +12,4 @@ END;
+--  /
+--  
+--  test111
+-- +m1
+-- END DIFF ----------------------------------
+
+
+
